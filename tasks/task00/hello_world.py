6b6f682e295b8b4433e70c3ade02e3b9c256fe15
Uliana <upimenova@exadel.com>
2021-09-11 15:39:18 +0300

    Task 01

--- tasks/task00/hello_world.py
+++ tasks/task00/hello_world.py
@@ -1,1 +1,18 @@
 print("Hello, World!")
+
+for k in range(1, 1001):
+    while k < 1001:
+        while k < 100:
+            while k < 10:
+                if k ** 1 == k:
+                    print(k, end=',')
+                k += 1
+                break
+            if k == ((k % 10) ** 2) + ((k // 10) ** 2):
+                print(k, end=',')
+            k += 1
+            break
+        if k == ((k % 100) ** 3 + (k // 100) ** 3) + ((k - k // 100 - k % 100) / 10 ** 3):
+            print(k, end=',')
+        k += 1
+        break
