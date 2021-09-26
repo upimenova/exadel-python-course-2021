f2f37fb17bac737169119e524039da91e95b6f85
Uliana <upimenova@exadel.com>
2021-09-26 22:43:45 +0300

    Task 04

--- tasks/task04/tree_leaves.py
+++ tasks/task04/tree_leaves.py
@@ -8,22 +8,36 @@ tree = {
    },
    "node2": [7, 8, 9]
 }
-leaves = []
 
 
 def collect_leaves(leaf):
-    if type(leaf) is dict:
-        for key, value in leaf.items():
-            if type(value) is dict:
-                collect_leaves(value)
-            else:
-                leaves.append(value)
-    else:
-        for i in leaf:
-            leaves.append(i)
-    return leaves
+    def _collect_leaves(leaf, result):
+        if type(leaf) is dict:
+            for key, value in leaf.items():
+                if type(value) is dict:
+                    _collect_leaves(value, result)
+                else:
+                    result.append(value)
+        else:
+            for i in leaf:
+                result.append(i)
+    res = list()
+    _collect_leaves(leaf, res)
+    return res
+
+# def collect_leaves(leaf):
+#     if type(leaf) is dict:
+#         for key, value in leaf.items():
+#             if type(value) is dict:
+#                 collect_leaves(value)
+#             else:
+#                 leaves.append(value)
+#     else:
+#         for i in leaf:
+#             leaves.append(i)
+#     return leaves
 
 
-# assert collect_leaves([1, 2, 3]) == [1, 2, 3], 'Your solution is incorrect!'
+assert collect_leaves([1, 2, 3]) == [1, 2, 3], 'Your solution is incorrect!'
 print(collect_leaves(tree))
-# collect_leaves(tree) == [1, 2, 3, 31, 5, 31, 7, 8, 9]
+# assert collect_leaves(tree) == [1, 2, 3, 31, 5, 31, 7, 8, 9]
