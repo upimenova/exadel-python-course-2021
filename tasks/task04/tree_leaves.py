c0375a5e3a7a8ecd0b22cac86eb225ca92f27b35
Uliana <upimenova@exadel.com>
2021-09-27 17:59:03 +0300

    Task 04

--- tasks/task04/tree_leaves.py
+++ tasks/task04/tree_leaves.py
@@ -14,30 +14,14 @@ def collect_leaves(leaf):
     def _collect_leaves(leaf, result):
         if type(leaf) is dict:
             for key, value in leaf.items():
-                if type(value) is dict:
-                    _collect_leaves(value, result)
-                else:
-                    result.append(value)
+                _collect_leaves(value, result)
         else:
-            for i in leaf:
-                result.append(i)
+            result += leaf
     res = list()
     _collect_leaves(leaf, res)
     return res
 
-# def collect_leaves(leaf):
-#     if type(leaf) is dict:
-#         for key, value in leaf.items():
-#             if type(value) is dict:
-#                 collect_leaves(value)
-#             else:
-#                 leaves.append(value)
-#     else:
-#         for i in leaf:
-#             leaves.append(i)
-#     return leaves
 
-
-assert collect_leaves([1, 2, 3]) == [1, 2, 3], 'Your solution is incorrect!'
 print(collect_leaves(tree))
-# assert collect_leaves(tree) == [1, 2, 3, 31, 5, 31, 7, 8, 9]
+assert collect_leaves([1, 2, 3]) == [1, 2, 3], 'Your solution is incorrect!'
+assert collect_leaves(tree) == [1, 2, 3, 31, 5, 31, 7, 8, 9], 'Your solution is incorrect!'
