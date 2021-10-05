f936a55a91c54e41f04b7feba3dd601e12e6eaa4
Uliana <upimenova@exadel.com>
2021-10-05 14:07:52 +0300

    Task 07

--- tasks/task07/orderRepository.py
+++ tasks/task07/orderRepository.py
@@ -5,7 +5,34 @@ class Good:
         self.price = price
 
 
-class Order:
+class OrderRepository:
+    order_repository = []
+
+    def add(self, order):
+        self.order_repository.append(order)
+
+    def get(self, order_id):
+        for i in self.order_repository:
+            if i.order_id == order_id:
+                return i
+            else:
+                print('There is no such order!')
+
+    def list(self, n_latest):
+        if n_latest is None:
+            print(self.order_repository)
+        else:
+            print(self.order_repository[:n_latest+1])
+
+    def delete(self, order_id):
+        for i in self.order_repository:
+            if i.order_id == order_id:
+                self.order_repository.remove(i)
+            else:
+                print('There is no such order!')
+
+
+class Order(OrderRepository):
     def __init__(self, order_id, order_date, client_id, goods, price):
         self.order_id = order_id
         self.order_date = order_date
@@ -14,21 +41,9 @@ class Order:
         self.price = price
 
 
-OrderRepository = []
-
-
-def add(order):
-    OrderRepository.append(order)
-
-
-def get(order_id):
-    for i in OrderRepository:
-        if i.order_id == order_id:
-            return i
-        else:
-            print('There is no such order!')
-
-
-first = Order(11, '12.12', 1, 'bread', 200)
-add(first)
-print(OrderRepository)
+first = Order(11, '12.12.2020', 1, 'bread', 200)
+second = Order(2, '01.08.2021', 3, 'cheese', 500)
+OrderRepository.add(OrderRepository, second)
+print(OrderRepository.get(OrderRepository, 2))
+OrderRepository.delete(OrderRepository, second)
+print(OrderRepository.get(OrderRepository, 2))
